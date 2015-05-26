b2009ae39874087268d1b404b2f4624d2d3f00be
madouh <tin.tin.nagi@gmail.com>
2015-05-26 17:13:07 +0200

    adding heroku sendgrid addon and getting its username and password directly and changing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV["SENDGRID_PASSWORD"],
-  :domain => "herokuapp.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
   :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
   # Code is not reloaded between requests.
