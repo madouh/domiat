adbb63625408a927e716bd084bed1e0d09b64eda
madouh <tin.tin.nagi@gmail.com>
2015-06-02 20:41:27 +0200

    modifying form input and adding SMS with twilio and localhost to domiat

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options ={host: 'localhost', port: 3000}# {host: 'domiat.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'domiat.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   
   # Code is not reloaded between requests.
