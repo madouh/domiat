f754e9e3e7a45d5be78b861b8c63b558a7aaaa91
madouh <tin.tin.nagi@gmail.com>
2015-07-29 17:23:43 +0200

    up to deleting puts

--- app/controllers/find_controller.rb
+++ app/controllers/find_controller.rb
@@ -3,12 +3,12 @@ class FindController < ApplicationController
   def search
   	@res=["Mamdouh","Ahmad","Sohair","Omar"]
   	@clean_params=(params[:search]).gsub(/\s+/, ' ')
-  	puts "from search ============================"
-  	puts @clean_params.inspect
-  	puts "============================"
+  	# puts "from search ============================"
+  	# puts @clean_params.inspect
+  	# puts "============================"
   	query="%" + @clean_params + "%"
   	@result=@verified_agent.where('name=?',@clean_params)
-  	puts @result.inspect
+  	# puts @result.inspect
   	@auto=[]
   	@auto.concat(@verified_agent.where('name LIKE ?', query).limit(5).pluck(:name))
   	@auto.concat(@verified_agent.where('activity LIKE ?', query).limit(5).pluck(:activity))
@@ -18,17 +18,17 @@ class FindController < ApplicationController
   	@auto.concat(@verified_agent.where('word4 LIKE ?', query).limit(5).pluck(:word4))
   	@auto.concat(@verified_agent.where('word5 LIKE ?', query).limit(5).pluck(:word5))
   	@auto.uniq!
-  	puts @auto.inspect
+  	# puts @auto.inspect
   end
   def show
     if params[:search]
         #@para=params
       	@clean_params=(params[:search]).gsub(/\s+/, ' ')
-      	puts "from show ============================"
-      	puts @clean_params.inspect
-      	puts "============================"
+      	# puts "from show ============================"
+      	# puts @clean_params.inspect
+      	# puts "============================"
       	@result=@verified_agent.where('name=? OR activity=? OR word1=? OR word2=? OR word3=? OR word4=? OR word5=?',@clean_params,@clean_params,@clean_params,@clean_params,@clean_params,@clean_params,@clean_params).page params[:page]
-      	puts @result.inspect
+      	# puts @result.inspect
         respond_to do |format|
             format.html 
             #format.json { render json: @result }
