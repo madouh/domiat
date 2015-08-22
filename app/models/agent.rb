9734dcd7d17684444a78180cbf8dfa5cdca9c561
madouh <tin.tin.nagi@gmail.com>
2015-08-23 00:48:43 +0200

    adding agent and user vaidations

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -3,38 +3,45 @@ class Agent < ActiveRecord::Base
 	#VALID_EMAIL_REGEX="/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i"
 	#before_save {self.email=email.downcase}
 	validates :name , :presence => { message:"اسم النشاط يجب أن يكون موجوداً"}, 
-					length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"} ,
-					 length:{minimum:10, message:"الاسم لا يقل عن 10 حروف"}
+					length:{maximum:15, message:"الاسم لا يزيد عن 15 حروف"} ,
+					 length:{minimum:6, message:"الاسم لا يقل عن 6 حروف"}
 
-validates :activity , :presence => { message:"اسم النشاط يجب أن يكون موجوداً"}, 
-					length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"} ,
-					 length:{minimum:10, message:"الاسم لا يقل عن 10 حروف"}
-validates :brief_of_activity , :presence => { message:"اسم النشاط يجب أن يكون موجوداً"}, 
-					length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"} ,
-					 length:{minimum:10, message:"الاسم لا يقل عن 10 حروف"}
-validates :street , :presence => { message:"اسم النشاط يجب أن يكون موجوداً"}, 
-					length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"} ,
-					 length:{minimum:5, message:"الاسم لا يقل عن 10 حروف"}
-validates :address , :presence => { message:"اسم النشاط يجب أن يكون موجوداً"}, 
-					length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"} ,
-					 length:{minimum:10, message:"الاسم لا يقل عن 10 حروف"}
-validates :region , :presence => { message:"اسم النشاط يجب أن يكون موجوداً"}, 
-					length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"} ,
-					 length:{minimum:10, message:"الاسم لا يقل عن 10 حروف"}
-validates :neighbour , :presence => { message:"اسم النشاط يجب أن يكون موجوداً"}, 
-					length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"} ,
-					 length:{minimum:10, message:"الاسم لا يقل عن 10 حروف"}
-validates :tel1 , :presence => { message:"اسم النشاط يجب أن يكون موجوداً"}, 
-					length:{maximum:13, message:"الاسم لا يزيد عن 25 حروف"} ,
-					 length:{minimum:11, message:"الاسم لا يقل عن 10 حروف"},
-					 :uniqueness => { message:"البريد الإلكتروني الذي أدخلته موجود مسبقاً"}
+validates :activity , :presence => { message:" النشاط يجب أن يكون موجوداً"}, 
+					length:{maximum:15, message:"النشاط لا يزيد عن 15 حروف"} ,
+					 length:{minimum:4, message:"النشاط لا يقل عن 4 حروف"}
 
-validates :latitude , :presence => { message:"اسم النشاط يجب أن يكون موجوداً"}, 
-					length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"} ,
-					 length:{minimum:10, message:"الاسم لا يقل عن 10 حروف"}
-validates :langitude , :presence => { message:"اسم النشاط يجب أن يكون موجوداً"}, 
-					length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"} ,
-					 length:{minimum:10, message:"الاسم لا يقل عن 10 حروف"}
+validates :brief_of_activity , :presence => { message:"مختصر النشاط يجب أن يكون موجوداً"}, 
+					length:{maximum:100, message:"مختصر النشاط لا يزيد عن 100 حروف"} ,
+					 length:{minimum:20, message:"مختصر النشاط لا يقل عن 20 حروف"}
+
+validates :street , :presence => { message:"اسم الشارع يجب أن يكون موجوداً"}, 
+					length:{maximum:15, message:"اسم الشارع لا يزيد عن 15 حروف"} ,
+					 length:{minimum:5, message:"اسم الشارع لا يقل عن 5 حروف"}
+
+validates :address , :presence => { message:"العنوان يجب أن يكون موجوداً"}, 
+					length:{maximum:25, message:"العنوان لا يزيد عن 25 حروف"} ,
+					 length:{minimum:10, message:"العنوان لا يقل عن 10 حروف"}
+
+validates :region , :presence => { message:"اسم المنطقة أو الحي يجب أن يكون موجوداً"}, 
+					length:{maximum:15, message:"اسم المنطقة أو الحي لا يزيد عن 15 حروف"} ,
+					 length:{minimum:5, message:"اسم المنطقة أو الحي لا يقل عن 5 حروف"}
+
+validates :neighbour , :presence => { message:"اسم المجاورة أو الناحية يجب أن يكون موجوداً"}, 
+					length:{maximum:15, message:"اسم المجاورة أو الناحية لا يزيد عن 15 حروف"} ,
+					 length:{minimum:5, message:"اسم المجاورة أو الناحية لا يقل عن 5 حروف"}
+
+validates :tel1 , :presence => { message:"رقم التليفون اﻷول يجب أن يكون موجوداً"}, 
+					length:{maximum:11, message:"رقم التليفون اﻷول لا يزيد عن 11 رقما و يجب ان يكون موبايل"} ,
+					 length:{minimum:11, message:"رقم التليفون اﻷول لا يقل عن 11 رقما و يجب ان يكون موبايل"},
+					 :uniqueness => { message:"رقم التليفون الذي أدخلته موجود مسبقاً"}
+
+validates :latitude , :presence => { message:"الاحداثي يجب أن يكون موجوداً"}, 
+					length:{maximum:15, message:"الاحداثي لا يزيد عن 15 رقم"} ,
+					 length:{minimum:10, message:"الاحداثي لا يقل عن 10 أرقام"}
+
+validates :langitude ,  :presence => { message:"الاحداثي يجب أن يكون موجوداً"}, 
+					length:{maximum:15, message:"الاحداثي لا يزيد عن 15 رقم"} ,
+					 length:{minimum:10, message:"الاحداثي لا يقل عن 10 أرقام"}
 
 
 	#validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
