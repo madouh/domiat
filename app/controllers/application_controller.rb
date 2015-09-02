822cc865d7452eb246e97694948e1a1a011ba217
madouh <tin.tin.nagi@gmail.com>
2015-09-02 20:43:36 +0200

    adding password confirmation for user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
     protected
 
         def configure_permitted_parameters
-            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password) }
-            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password,:password_confirmation) }
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password,:password_confirmation) }
         end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@
 class User < ActiveRecord::Base
 	before_save {self.email=email.downcase}
 	validates :username , :presence => { message:"الاسم يجب أن يكون موجوداً"}
-	 , length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"}
-	 , length:{minimum:8, message:"الاسم لا يقل عن 10 حروف"}
-   validates_confirmation_of :password
+	validates :username , length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"}
+	validates :username , length:{minimum:8, message:"الاسم لا يقل عن 10 حروف"}
+
 	has_many :agents
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
