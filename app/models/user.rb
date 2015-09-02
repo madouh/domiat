b07c0ee402bf5242b90169367e86bc0f3b461b7b
madouh <tin.tin.nagi@gmail.com>
2015-09-02 19:12:36 +0200

    adding password confirmation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@
 class User < ActiveRecord::Base
 	before_save {self.email=email.downcase}
 	validates :username , :presence => { message:"الاسم يجب أن يكون موجوداً"}
-	validates :username , length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"}
-	validates :username , length:{minimum:10, message:"الاسم لا يقل عن 10 حروف"}
-
+	 , length:{maximum:25, message:"الاسم لا يزيد عن 25 حروف"}
+	 , length:{minimum:8, message:"الاسم لا يقل عن 10 حروف"}
+   validates_confirmation_of :password
 	has_many :agents
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
